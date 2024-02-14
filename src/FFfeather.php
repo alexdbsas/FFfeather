81d879d13998e47417fd790e5c20f5af1bfbe6e3
AlexD <5991205+alexdbsas@users.noreply.github.com>
2024-02-14 15:54:57 -0300

    v1.0.3 fixed cache issue relay attributes now

--- src/FFfeather.php
+++ src/FFfeather.php
@@ -12,36 +12,41 @@ class FFfeather
 {
     private \Base $f3;
     private \Template $tpl;
-    private string $base;
     private string $sprite = 'feather-icon.svg';
     private string $src = 'https://unpkg.com/feather-icons/dist/feather-sprite.svg';
     private string $path;
 
     public function __construct()
     {
-        $this->base = __DIR__;
-        $this->path = implode(DIRECTORY_SEPARATOR, [$this->base, $this->sprite]);
+        $this->f3 = \Base::instance();
+        $this->path = $this->f3->get('UI') . $this->sprite;
         if (!file_exists($this->path)) {
             $content = file_get_contents($this->src);
             file_put_contents($this->path, $content);
         }
-        $this->f3 = \Base::instance();
+        $this->f3->route("GET /{$this->sprite}", [$this, 'getSprite'], 31536000);
         $this->tpl = \Template::instance();
         $this->tpl->extend('feather', [$this, 'renderTag']);
-        $this->f3->route("GET /{$this->sprite}", [$this, 'getSprite']);
     }
 
     public function getSprite()
     {
-        $this->f3->status(200);
-        header('Content-type: application/xml');
-        echo (file_get_contents($this->path));
+        $cache = \Cache::instance();
+        echo $this->tpl->render($this->sprite, "image/svg+xml", null, 31536000);
     }
 
     public function renderTag($args)
     {
         $attr = $args['@attrib'];
+        $list = [];
+        $classes = [];
         $type = $attr['type'];
-        return "<svg class=\"feather\"><use href=\"/{$this->sprite}#$type\"/></svg>";
+        foreach ($attr as $key => &$value) {
+            if ($key === 'class') $classes[] = $value;
+            elseif ($key !== 'type') $list[] = "$key=\"$value\"";
+        }
+        $row = ' ' . implode(' ', $list);
+        $classes = $classes ? implode(' ', $classes) : 'feather';
+        return "<svg$row class=\"$classes\"><use href=\"/{$this->sprite}#$type\"/></svg>";
     }
 }
